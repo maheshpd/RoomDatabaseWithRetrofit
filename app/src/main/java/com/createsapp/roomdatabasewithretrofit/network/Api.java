b20182093132cb60b9fcb3993d3adce7059d2aef
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-29 00:18:52 +0530

    Done

--- app/src/main/java/com/createsapp/roomdatabasewithretrofit/network/Api.java
+++ app/src/main/java/com/createsapp/roomdatabasewithretrofit/network/Api.java
@@ -7,7 +7,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-interface Api {
-    @GET("data.php")
+public interface Api {
+    @GET("data.php/")
     Call<List<Actor>> getAllActors();
 }
