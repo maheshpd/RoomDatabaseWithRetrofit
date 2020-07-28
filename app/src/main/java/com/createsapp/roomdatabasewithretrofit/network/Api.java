352479c512945150eee6c13ab90b06819afd88ba
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-07-28 22:13:58 +0530

    Create Api and XML

--- app/src/main/java/com/createsapp/roomdatabasewithretrofit/network/Api.java
+++ app/src/main/java/com/createsapp/roomdatabasewithretrofit/network/Api.java
@@ -1,4 +1,13 @@
 package com.createsapp.roomdatabasewithretrofit.network;
 
+import com.createsapp.roomdatabasewithretrofit.model.Actor;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 interface Api {
+    @GET("data.php")
+    Call<List<Actor>> getAllActors();
 }
